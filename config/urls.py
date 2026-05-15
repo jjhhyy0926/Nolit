8d9017b6228698f379148d8bf41ac12b6ea33ea2
minhakim <leedhroxx@gmail.com>
2026-05-15 16:32:31 +0900

    chore: remove pycache files

--- config/urls.py
+++ config/urls.py
@@ -17,12 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/",         admin.site.urls),
-    path("", TemplateView.as_view(template_name="index.html"), name="index"),
+    path("", include("recommender.urls")),
     path("accounts/",      include("accounts.urls",     namespace="accounts")),
     path("recommender/",   include("recommender.urls",  namespace="recommender")),
     path("contents/",      include("contents.urls",     namespace="contents")),
